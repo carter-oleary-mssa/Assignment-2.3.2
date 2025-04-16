7e28b94e679f2c447cfb6b8c030494fbd27c31d1
carter-oleary <carter.m.oleary@outlook.com>
2025-04-16 11:07:16 -0400

    Added comments

--- Program.cs
+++ Program.cs
@@ -6,13 +6,19 @@ char ans;
 do
 {
     Console.WriteLine("-------- TIP CALCULATOR -----------");
+
+    // Get initial bill total and desired tip percentage
     Console.Write("Enter bill total: ");
     decimal bill = Convert.ToDecimal(Console.ReadLine());
     Console.Write("Enter tip %: ");
     decimal tipPercent = Convert.ToDecimal(Console.ReadLine());
+
+    // Calculate tip amount and print formatted total
     decimal tipAmount = bill * (tipPercent/100);
     Console.WriteLine($"Your tip is {(tipAmount).ToString("C", ci)}");
     Console.WriteLine($"The grand total for your bill is: {(bill + tipAmount).ToString("C", ci)}");
+
+    // Check for exit condition from user
     while (true)
     {
         Console.WriteLine("\nWould you like to calculate another bill? (y/n)");
